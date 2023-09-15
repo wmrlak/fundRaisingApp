7501ed55e825bd46a098f4d8ca095650129dcce3
Stratos Doukakis <stratosver2@gmail.com>
2023-09-15 14:48:14 +0100

    Fix compile issue in Campaign.test.js

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -2,7 +2,7 @@
 
 const assert = require('assert');
 const ganache = require('ganache-cli');
-const { Web3 } = require('web3');
+const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
 const compiledFactory = require('../ethereum/build/CampaignFactory.json');
