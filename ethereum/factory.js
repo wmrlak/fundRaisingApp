0c8f35ddcfef2f65e4521b293f0b08ccd77b1692
Stratos Doukakis <stratosver2@gmail.com>
2023-09-15 14:49:17 +0100

    Change the address of the deployed contract.

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -7,7 +7,7 @@ const instance = new web3.eth.Contract(
 	CampaignFactory.abi,
 
 	//address of the most recently deployed contract
-	'0x3D2b4E0C81B1D5Fb7347e5034483F131032bAa82'
+	'0x0Ef0720321b8F39E971947B191F17Ebb277DE15B'
 );
 
 export default instance;
